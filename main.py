6734799434eb0813221d37cdf64fa8b2ccd5b383
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-05-23 20:40:26 -0500

    basic program with user input complete

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("What are your initials?\n")
+pet = input("What is your favorite pet's name?\n")
+color = input("What is your favorite color?\n")
+food = input('What is your favorite dish to eat?\n')
+tag = ''+color+food+pet+name+''
+print('\n\nYour gamer tag name is:\n\n\n '+ tag +'\n\n\n')
+print('Now get to gaming 👾')
