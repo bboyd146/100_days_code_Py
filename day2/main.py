892e2275a418d1b59f6e8590a800e11b6d03ee88
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-06-01 20:11:56 -0500

    added method to round to nearest tenth and used fstring

--- day2/main.py
+++ day2/main.py
@@ -1,5 +1,5 @@
 total = float(input('What is the bill total? $'))
-tip = int(input('How much would you like to tip? 10 15 20? '))
+tip = int(input('How much would you like to tip? 10 15 20?\n (do not add % sign) '))
 bill_w_tip = tip / 100 * total + total
 
-print(bill_w_tip)
+print(f'Your total bill including tip will be:\n ${bill_w_tip:.2f}')
