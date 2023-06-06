935d68768c9705148ecf472aa9ed3e4c289db024
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-06-05 19:10:21 -0500

    still working on more performant code, but moving on to day 3

--- day2/main.py
+++ day2/main.py
@@ -1,5 +1,16 @@
+print('Welcome to the Tip Calculator !\n\n\n')
 total = float(input('What is the bill total? $'))
-tip = int(input('How much would you like to tip? 10 15 20?\n (do not add % sign) '))
-bill_w_tip = tip / 100 * total + total
+tip_per = int(
+    input('How much would you like to tip? 10 15 20?\n (do not add % sign) '))
+bill_w_tip = (tip_per / 100 * total) + total
+
+other_diners = ['yes', 'no']
+
+
+def question_for_people():
+    for x in other_diners:
+        print(x)
+
 
 print(f'Your total bill including tip will be:\n ${bill_w_tip:.2f}')
+# print(question_for_people())
