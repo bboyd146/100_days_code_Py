2efccca42309252d71d4b9f4324c0f6b0c608b48
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2023-06-05 20:13:41 -0500

    first conditional done

--- day3/main.py
+++ day3/main.py
@@ -22,3 +22,13 @@ print('''
   ~-_         _-~ /_______________________________/    `-_         _-~
      ~ ----- ~                                            ~ ----- ~  -TX
 ''')
+print('Welcome to Death Ride 😈🩼🚑\n\n')
+print('Your mission is to get through the race at super high speed without any accidents.')
+print("You're moving at high speeds, so there is no time to think! 🙀\n\n")
+
+first_turn= input("The race has started - You've hit the first corner - Will you lean right or lean left?\n Type R or L.")
+if first_turn == 'R':
+    print("Nice turn!\n You've got the lead")
+    
+else: 
+    print('OUCH 🤕!!\n You took a sketchy shortcut that led you over a cliff.\n YOU LOSE')
