1c809581aba619d10ac875702acd7ba26492519c
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2025-01-22 13:45:24 -0600

    initial code for password generator

---
+++ day5/main.py
@@ -0,0 +1,7 @@
+import random
+print('Welcome to the PyPassword Generator:\n\n')
+letters_choice = int(input("How many letters would you like in your password?\n"))
+symbols_choice = int(input("How many symbols would you like?\n"))
+nums_choice = int(input("How many numbers would you like?\n"))
+
+user_choices = [letters_choice, symbols_choice, nums_choice]
