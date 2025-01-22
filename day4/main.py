3abc54ed8056ebfb97b322421a705c037f0d7d99
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2025-01-22 13:28:20 -0600

    updated code to output ASCII art in outputs

--- day4/main.py
+++ day4/main.py
@@ -1,4 +1,6 @@
 import random
+
+# ASCII art for rock, paper, and scissors
 rock = '''
     _______
 ---'   ____)
@@ -26,13 +28,29 @@ scissors = '''
 ---.__(___)
 '''
 
-user_choice = int(input("What would you like to choose? Type 0 for Rock, 1 for Paper, 2 for scissors."))
-computer_choice= random.randint(0,2)
-print(f"Computer chose: {computer_choice}")
+# List of choices for easy access
+choices = [rock, paper, scissors]
 
-if computer_choice == 0 and user_choice == 1:
-    print("You win!")
-elif computer_choice > user_choice:
-    print("You lose!")
-else : 
-    print("Tie")
+# Get user choice
+user_choice = int(input("What would you like to choose? Type 0 for Rock, 1 for Paper, 2 for Scissors: "))
+if user_choice < 0 or user_choice > 2:
+    print("Invalid choice. You lose!")
+else:
+    # Get computer choice
+    computer_choice = random.randint(0, 2)
+
+    # Display the choices
+    print("\nYou chose:")
+    print(choices[user_choice])
+    print("Computer chose:")
+    print(choices[computer_choice])
+
+    # Determine the result
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == 0 and computer_choice == 2) or \
+         (user_choice == 1 and computer_choice == 0) or \
+         (user_choice == 2 and computer_choice == 1):
+        print("You win!")
+    else:
+        print("You lose!")
