91dcdc5a9c4f126ccec46126fe12a5faf6ef2673
bboyd146 <82745040+bboyd146@users.noreply.github.com>
2025-01-22 14:20:37 -0600

    added for loop practice

--- day5/main.py
+++ day5/main.py
@@ -5,3 +5,7 @@ symbols_choice = int(input("How many symbols would you like?\n"))
 nums_choice = int(input("How many numbers would you like?\n"))
 
 user_choices = [letters_choice, symbols_choice, nums_choice]
+letters =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+upper_letters= [letter.upper() for letter in letters]
+numbers= [0,1,2,3,4,5,6,7,8,9]
+print(upper_letters)
---
+++ day5/task.py
@@ -0,0 +1,15 @@
+# Range Function
+
+for number in range(1,11):
+    print(number)
+
+# Range with last parameter indicating step/count
+for number in range(1,11,2):
+    print(number)
+
+# Add numbers 1 to 100 all together
+sum=0
+for number in range(1,101):
+    sum += number
+    
+print(sum)
